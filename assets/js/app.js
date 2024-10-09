0139ff14eedc94c5461b05839867ba56230f72e6
Tiegow <tiegorafael@gmail.com>
2024-10-08 22:30:34 -0300

    feat: página de vídeos, biblioteca de carrossel e cores

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,8 @@ function loadPage(page) {
         .then(response => response.text())
         .then(html => {
             document.getElementById('content').innerHTML = html;
+
+            loadFlickity();
         });
 
     // Carregar o CSS específico da página
@@ -25,6 +27,36 @@ function loadComponents() {
         .then(html => {
             document.getElementById('footer').innerHTML = html;
         });
+}
+
+function loadFlickity() {
+    // Carregar o CSS do Flickity
+    const flickityCSS = document.createElement('link');
+    flickityCSS.rel = 'stylesheet';
+    flickityCSS.href = 'https://cdnjs.cloudflare.com/ajax/libs/flickity/2.3.0/flickity.min.css';
+    document.head.appendChild(flickityCSS);
+
+    // Carregar o JS do Flickity
+    const flickityJS = document.createElement('script');
+    flickityJS.src = 'https://cdnjs.cloudflare.com/ajax/libs/flickity/2.3.0/flickity.pkgd.min.js';
+    flickityJS.onload = function() {
+        // Selecionar todos os carrosséis na página
+        const carousels = document.querySelectorAll('.carousel');
+        
+        // Aplicar o Flickity em cada carrossel
+        carousels.forEach(function(carousel) {
+            new Flickity(carousel, {
+                cellAlign: 'left',
+                contain: true,
+                wrapAround: true,
+                autoPlay: 3000,
+                pauseAutoPlayOnHover: false,
+                pageDots: false,
+                prevNextButtons: false
+            });
+        });
+    };
+    document.body.appendChild(flickityJS);
 }
 
 // Inicializar a SPA com a página inicial
